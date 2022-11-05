7f0ac511725e626bf5bdf3c8c152612de7a5e5c8
nik-baker <nik.baker@hotmail.com>
2022-11-05 12:47:54 -0700

    Clean up app.js file and add comments

--- app.js
+++ app.js
@@ -1,41 +1,57 @@
+// All of the Packages We Require
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
-mongoose.set('useFindAndModify', false);
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
+const mongoose = require('mongoose');
+
+// Data Depreciation
+mongoose.set('useFindAndModify', false);
+
+// Error Handling
 const ExpressError = require('./utils/ExpressError');
+
+// So we can make requests other than .get
 const methodOverride = require('method-override');
+
+// Lets us Call Files from Other Directories 
 const { join } = require('path');
 
+// Our Router Routes
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
 
+// Connect to Mongoose
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
 
+// Connect to Mongo Database 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
 
+// Turn on Express
 const app = express();
 
-
+// Setting our View Engine
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+// Turning on Our Configurations we Included
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 
 
+// Using our Routes
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
+
 
 
 // THIS IS THE HOME PAGE
