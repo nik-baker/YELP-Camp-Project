dd60572cca0a1583d0fbdea4093292b5431a283e
nik-baker <nik.baker@hotmail.com>
2022-11-09 10:15:28 -0800

    Add Authentication to App

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 
 
@@ -18,8 +21,9 @@ const methodOverride = require('method-override');
 const { join } = require('path');
 
 // Our Router Routes
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
+const userRoutes = require('./routes/users')
 
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -44,11 +48,12 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-// Turning on Our Configurations we Included
+// Turning on Our Configurations we Included (Our Middleware)
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configuring our Session 
 const sessionConfig = {
     secret: "bettersecret",
     resave: false,
@@ -59,10 +64,23 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7,
     }
 }
+
+// Turning on Session and Flash 
 app.use(session(sessionConfig))
 app.use(flash());
 
+// Turn on Passport 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+// How passport handles user data
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Things we use from Passport Locals Packager (Flash & User info)
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
     next();
@@ -70,8 +88,9 @@ app.use((req, res, next) => {
 
 
 // Using our Routes
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+app.use('/', userRoutes)
 
 
 
