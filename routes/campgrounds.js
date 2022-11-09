ea72f2f9f8f5b1f361d9f9f0d478fcd8a00b93eb
nik-baker <nik.baker@hotmail.com>
2022-11-09 10:15:50 -0800

    Add Authentication to App

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,6 +4,8 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
+
 
 const validateCampground = (req, res, next,) => {
     const { error } = campgroundSchema.validate(req.body);
@@ -18,23 +20,23 @@ const validateCampground = (req, res, next,) => {
 
 
 // THIS IS THE INDEX PAGE 
-router.get('/', async (req, res) => {
+router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
-});
+}));
 
 // THIS IS THE CREATE A NEW CAMPGROUND PAGE
-router.get('/new', catchAsync(async (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
-}))
+})
 
 // THIS IS THE POST FOR CREATE A NEW CAMPGROUND
-router.post('/', validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
-}))
+}));
 
 // THIS IS THE SHOW PAGE FOR AN INDIVIDUAL CAMPGROUND
 router.get('/:id', catchAsync(async (req, res,) => {
@@ -47,7 +49,7 @@ router.get('/:id', catchAsync(async (req, res,) => {
 }));
 
 // THIS IS THE EDIT PAGE
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     if (!campground) {
         req.flash('error', 'Campground not found')
@@ -57,7 +59,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 }))
 
 // THIS SUBMITS THE NEW DATA FROM EDIT TO THE DATA BASE 
-router.put('/:id', validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated a campground!')
@@ -65,7 +67,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 }));
 
 // THIS IS THE DELETE FUNCTIONALITY 
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground!')
