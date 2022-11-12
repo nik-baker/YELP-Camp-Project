ab7bd55160278144686b0fba19f15fb451005444
nik-baker <nik.baker@hotmail.com>
2022-11-11 19:25:27 -0800

    Everything works here

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,11 +11,11 @@ const upload = multer({ storage });
 // THIS IS THE INDEX PAGE & POST FOR CREATE A NEW CAMPGROUND
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.array('image'), (req, res) => {
-        console.log(req.body, req.files);
-        res.send("It worked")
-    })
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+// .post(upload.array('image'), (req, res) => {
+//     console.log(req.body, req.files);
+//     res.send("It worked")
+// })
 
 
 // THIS IS THE CREATE A NEW CAMPGROUND PAGE
